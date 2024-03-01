9b676825625f4487961009e3d659b54c76719a63
Jiawei Wang <jiaweiwang@Jiaweis-Mac-mini.local>
2024-03-01 13:12:43 -0800

    test get Compnay info

--- client/src/services/getCompanyInfo.js
+++ client/src/services/getCompanyInfo.js
@@ -5,6 +5,7 @@ const handleGetCompanyInfo = async () => {
       const response = await fetch('/getCompanyInfo'); // 调整为您的后端地址，如果后端和前端不在同一域，需要完整的 URL
       if (!response.ok) throw new Error('Failed to fetch company info');
       const data = await response.json();
+      console.log('hit the getCompanyInfo')
       console.log(data);
       // 在这里处理返回的公司信息，例如显示到 UI 中
     } catch (error) {
